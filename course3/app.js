fde57ad64bf546450b53885abb15c80b0ffe8c53
Refs <lhx1010339808@outlook.com>
2018-04-03 11:51:54 +0800

    .

--- course3/app.js
+++ course3/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products');
+const orderRoutes =  require('./api/routes/orders');
 
-/**
- * A route will match any path that follows its path immediately with a “/”. For example: app.use('/apple', ...) will match “/apple”, “/apple/images”, “/apple/images/news”, and so on.
- * 
- * Since path defaults to “/”, middleware mounted without a path will be executed for every request to the app.
-For example, this middleware function will be executed for every request to the app:
-*/
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+
+app.use('/products', productRoutes);
+// app.use('/orders', orderRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
