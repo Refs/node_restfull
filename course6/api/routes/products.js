679b053edfcdc9be5c3abad3d9661bcc24fcc9fd
Refs <lhx1010339808@outlook.com>
2018-04-04 14:42:52 +0800

    .

---
+++ course6/api/routes/products.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+
+// router.use(
+//     (req, res, next) => {
+//         res.status(200).json({
+//         message: "Product Router works!"
+//         })
+//     }
+// )
+
+/**
+ * A router object is an isolated instance of middleware and routes. You can think of it as a “mini-application,” capable only of performing middleware and routing functions. Every Express application has a built-in app router.
+ * 
+ * 1. router 仅用来 挂在中间件，将匹配的请求，路由到某个函数中；
+ * 2. app  之所以可以挂在路由或路由function 是因为其内置一个router; 
+*/
+
+router.get('/',(req, res, next) => {
+    res.status(200).json({
+        message: 'Handing GET requests to /products'
+    })
+})
+router.post('/',(req, res, next) => {
+
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
+    // 新建的状态码一般是201
+    res.status(201).json({
+        message: 'Handing POST requests to /products',
+        createdProduct: product
+    })
+})
+
+router.get('/:productID',(req,res,next)=>{
+    const id = req.params.productID;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'you discover the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'you pass an ID',
+            id: id
+        })
+    }
+})
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
+
+
+module.exports = router;
---
+++ course6/app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes =  require('./api/routes/orders');
+
+/**
+ * use the log middleware -- morgan
+*/
+app.use(morgan('dev'));
+
+/**
+ * use the body-parser middleware
+ * Content-Type 内容类型，用于定义网络文件的类型和网页的编码，决定浏览器将以什么形式、什么编码读取这个文件（浏览器会据此决定如何去显示返回的内容）
+*/
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json());
+
+
+app.use((req,res,next)=>{
+    res.header('Allow-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Request-With, Content-Type, Accept, Authorization'
+    )
+    if (req.method === 'OPTIONS') {
+        res.header(
+            'Acess-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+        )
+        return res.status(200).json({});
+    }
+})
+
+
+app.use('/products', productRoutes);
+// app.use('/orders', orderRoutes);
+app.use('/orders', orderRoutes);
+
+/**
+ * handle the error request
+ * 
+*/
+app.use(
+    (req,res,next) => {
+        const error = new Error('Not Found');
+        error.status = 404;
+        next(error);
+    }
+)
+
+app.use(
+    (error,req,res,next) => {
+        res.status(error.status || 500);
+        res.json({
+            error: {
+                message: error.message
+            }
+        })
+    }
+)
+
+module.exports = app;
