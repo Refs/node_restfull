891aa5b0b3ed947ea8b54566bed0285f05f50b65
Refs <lhx1010339808@outlook.com>
2018-04-10 15:10:09 +0800

    .

--- course7/api/routes/products.js
+++ course7/api/routes/products.js
@@ -19,22 +19,39 @@ mongoose.Promise = global.Promise;
  * 
  * 1. router 仅用来 挂在中间件，将匹配的请求，路由到某个函数中；
  * 2. app  之所以可以挂在路由或路由function 是因为其内置一个router; 
-*/
+ */
 
-router.get('/',(req, res, next) => {
-    ProductModel.findAllProducts().then(
-        (value) => {
-            res.status(200).json(value);
-        }
-    )
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
+router.get('/', (req, res, next) => {
+    ProductModel.findAllProducts().select("name price _id").then(
+            (docs) => {
+                const response = {
+                    count: docs.length,
+                    products: docs.map(
+                        doc => {
+                            return {
+                                name: doc.name,
+                                price: doc.price,
+                                _id: doc._id,
+                                request: {
+                                    type: "GET",
+                                    url: "http://127.0.0.1:3000/products/" + doc._id
+                                }
+                            }
+                        }
+                    )
+                }
+
+                res.status(200).json(response);
+            }
+        )
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
         });
-    });
 })
-router.post('/',(req, res, next) => {
+router.post('/', (req, res, next) => {
 
     const product = new ProductModel({
         _id: new mongoose.Types.ObjectId(),
@@ -43,32 +60,44 @@ router.post('/',(req, res, next) => {
     })
 
     ProductModel.createProduct(product).then(
-        () => {
-            console.log('数据已经存储进去了');
-            return ProductModel.findAllProducts()
-        }
-    ).then(
-        (value) => {
-            res.status(200).json(value);
-        }
-    )
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
+            (result) => {
+                res.status(201).json({
+                    message: "Created product successfully",
+                    createdProduct: {
+                        name: result.name,
+                        price: result.price,
+                        _id: result._id,
+                        request: {
+                            type: "GET",
+                            url: "http://127.0.0.1:3000/products/" + result._id
+                        }
+                    }
+                });
+            }
+        )
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
         });
-    });
 
 
 })
 
-router.get('/:productID',(req,res,next)=>{
+router.get('/:productID', (req, res, next) => {
     const id = req.params.productID;
     ProductModel.findProductById(id)
         .then(
-            (value) => {
-                if (value) {
-                    res.status(200).json(value);
+            (doc) => {
+                if (doc) {
+                    res.status(200).json({
+                        product: doc,
+                        request: {
+                            type: "GET",
+                            url: "http://127.0.0.1:3000/products"
+                        }
+                    });
                 } else {
                     res
                         .status(404)
@@ -81,7 +110,7 @@ router.get('/:productID',(req,res,next)=>{
         .catch(err => {
             console.log(err);
             res.status(500).json({
-              error: err
+                error: err
             });
         });
 })
@@ -92,17 +121,22 @@ router.patch('/:productId', (req, res, next) => {
     for (let ops in req.body) {
         updateOps[ops] = req.body[ops];
     }
-    ProductModel.updateProductById(productId, updateOps )
+    ProductModel.updateProductById(productId, updateOps)
         .then(
             (result) => {
-                console.log(result);
-                res.status(201).json(result);
+                res.status(201).json({
+                    message: 'Product updated',
+                    request: {
+                        type: 'GET',
+                        url: 'http://127.0.0.1:3000/products/' + productId
+                    }
+                });
             }
         )
         .catch(err => {
             console.log(err);
             res.status(500).json({
-              error: err
+                error: err
             });
         });
 });
@@ -114,16 +148,26 @@ router.delete('/:productId', (req, res, next) => {
         .then(
             (value) => {
                 console.log(value);
-                res.status(200).json(value);
+                res.status(200).json({
+                    message: 'Product deleted',
+                    request: {
+                        type: 'POST',
+                        url: 'http://localhost:3000/products',
+                        body: {
+                            name: 'String',
+                            price: 'Number'
+                        }
+                    }
+                });
             }
         )
         .catch(err => {
             console.log(err);
             res.status(500).json({
-              error: err
+                error: err
             });
         });
-    
+
 });
 
 
