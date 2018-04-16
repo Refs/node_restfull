fc5531f2fd119b7f0fd80b39c36fa535b7f75e76
Refs <lhx1010339808@outlook.com>
2018-04-16 16:00:10 +0800

    .

--- course12/api/models/user.model.server.js
+++ course12/api/models/user.model.server.js
@@ -1,5 +1,7 @@
+
 const mongoose =  require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = require("../shemas/user.schema.server");
 
@@ -16,12 +18,29 @@ function hashPassword ( password ) {
     return bcrypt.hash( password , 10 )
 }
 
+function comparePassword (clientPassword, dbPassword) {
+    return bcrypt.compare(clientPassword, dbPassword)
+}
+
+function createToken(payload, secretOrPrivateKey,options) {
+    return new Promise( (resolve, reject) => {
+        jwt.sign(payload, secretOrPrivateKey, options, (err,token) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(token);
+            }
+        })
+    } )
+}
+
 function createUser (user) {
     return UserModel.create(user)
 }
 
 UserModel.findUser = findUser;
 UserModel.hashPassword = hashPassword;
+UserModel.comparePassword = comparePassword;
 UserModel.createUser = createUser;
 
 
