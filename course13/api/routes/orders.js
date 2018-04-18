306d48118d8f0abe243c92b8c40946be0ec83d84
Refs <lhx1010339808@outlook.com>
2018-04-18 08:30:48 +0800

    .

---
+++ course13/api/routes/orders.js
@@ -0,0 +1,152 @@
+var mongoose =  require('mongoose');
+var express = require('express');
+var OrderModel = require('../models/order.model.server');
+var ProductModel = require('../models/product.model.server');
+var router = express.Router();
+mongoose.Promise = global.Promise;
+
+router.get('/', (req, res, next) => {
+    OrderModel
+        .findAllOrder()
+        .select("productId quantity _id")
+        .populate('productId','name price')
+        .then(
+            docs =>{
+                res.status(200).json({
+                    count: docs.length,
+                    orders: docs.map( doc => {
+                        return {
+                            _id: doc._id,
+                            productId: doc.productId,
+                            request: {
+                                type: 'GET',
+                                url: 'http://127.0.0.1:3000/orders/' + doc._id
+                            }
+                        }
+                    } )
+                })
+            }
+        )
+        .catch(
+            err => {
+                res.status(500).json({
+                    error: err
+                })
+            }
+        )
+});
+
+// 生成订单的时候，需要提供产品的相关信息，包块产品的_id 以及 产品的数量，即发送生成订单的请求的时候，应该去携带这两部分的信息
+
+router.post('/', (req, res, next) => {
+
+    var productId = req.body.productId;
+    var quantity = req.body.quantity;
+
+    ProductModel.findProductById( productId )
+        .then(
+            // 我们应该去手动处理，product不存在的情况，因为默认情况西 mongodb若找不到product 其不会报错，而只会返回一个null;
+            (product) => {
+                if ( ! product ) {
+                    res.status(404)
+                        .json({
+                            message: "coundn't find spec product "
+                        })
+                } 
+
+                var order = {
+                    _id : mongoose.Types.ObjectId(),
+                    productId: product._id,
+                    quantity: quantity,
+                }
+                return OrderModel.createOrder(order)
+            }
+        ).then(
+            result => {
+                res.status(201)
+                    .json({
+                        message: "Order stored",
+                        createdOrder: {
+                            _id: result._id,
+                            productId: result.productId,
+                            quantity: result.quantity
+                        },
+                        request: {
+                            type: 'GET',
+                            url: "127.0.0.1:3000/orders/" + result._id
+                        }
+                    })
+            }
+        )
+        .catch(
+            err => {
+                res.status(500).json({
+                    errMessage: err
+                })
+            }
+        );
+});
+
+router.get('/:orderId', (req, res, next) => {
+    var orderId = req.params.orderId;
+
+    OrderModel.findOrderById(orderId)
+        .populate('productId')
+        .then(
+            order => {
+                if (! order) {
+                    res.status(404)
+                        .json({
+                            message: 'Order not found'
+                        })
+                }
+                res.status(200)
+                    .json({
+                        order: order,
+                        request: {
+                            type: 'GET',
+                            url: "http://127.0.0.1:3000/orders"
+                        }
+                    })
+            }
+        )
+        .catch(
+            err => {
+                res.status(500)
+                    .json({
+                        error: err
+                    })
+            }
+        )
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    var orderId = req.params.orderId;
+
+    OrderModel.deleteOrderById(orderId)
+        .then(
+            result => {
+                res.status(200)
+                    .json({
+                        message: 'order deleted',
+                        request: {
+                            type: 'POST',
+                            url: '127.0.0.1:3000/orders',
+                            body: {
+                                productId: 'ID',
+                                quantity: 'Number'
+                            }
+                        }
+                    })
+            }
+        )
+        .catch(
+            err => {
+                res.status(500).json({
+                    error: err
+                })
+            }
+        )
+});
+
+module.exports = router;
---
+++ course13/api/routes/users.js
@@ -0,0 +1,161 @@
+var express = require('express');
+var mongoose =  require('mongoose');
+var router = express.Router();
+var UserModel = require('../models/user.model.server');
+
+
+mongoose.Promise = global.Promise;
+
+
+router.post('/signup', (req,res,next)=> {
+   UserModel.findUser(req.body.email)
+        .then(
+            (user) => {
+                if (user.length >= 1) {
+                    // res.status(409)
+                    //     .json({
+                    //         message: "Mail Already Exists"
+                    //     });
+                    // 此处需要运用 `中断或取消 Promise 链 的技术`，否则 当前的then 执行完毕之后，后续的then 一样会继续执行； 导致node 出现，res 已经返回，不能再修改res的错误；具体看自己的readme文档； 
+                    // 此处手动 抛错，直接抛到尾端的额catch() 里面去执行；
+                    // throw new Error('Mail Already Exists', status)  ----> 手动抛错
+                    var repeatError = new Error('');
+                    repeatError.message = 'Mail Already Exists';
+                    // 利用+ 转化为数字  利用!! 转化为布尔值
+                    repeatError.status = +409;
+
+                    throw repeatError;
+
+                } else {
+                    return UserModel.hashPassword(req.body.password);
+                }
+            }
+        )
+        
+        .then(
+            (hash) => {
+                const user = new UserModel({
+                    _id: new mongoose.Types.ObjectId(),
+                    email: req.body.email,
+                    password: hash
+                });
+                return UserModel.createUser(user)
+            }
+        )
+        .then(
+            ( result ) => {
+                console.log(result);
+                res.status(200)
+                    .json({
+                        message: 'User created'
+                    })
+            }
+        )
+        .catch(
+            (err) => {
+                if (err.status) {
+                    // 接收手动抛出的错误！
+                    res.status(err.status)
+                        .json({
+                            message: err.message
+                        })
+                } else {
+                    res.status(500)
+                    .json({
+                        error: err
+                    })
+                }
+            }
+        )
+
+})
+
+router.post('/login', (req,res,next) => {
+    let loginUser;
+    UserModel.findUser(req.body.email)
+        .then(
+            (user) => {
+                if (user.length < 1) {
+                    var authFailError = new Error('');
+                    authFailError.message = 'Auth Failed';
+                    authFailError.status = +401;
+                    throw authFailError;
+                } else {
+                    loginUser = user[0];
+                    return UserModel.comparePassword(req.body.password, user[0].password)
+                }
+            }
+        )
+        .then(
+            (result) => {
+                if (! result) {
+                    var authFailError = new Error('');
+                    authFailError.message = 'Auth Failed';
+                    authFailError.status = +401;
+                    throw authFailError;
+                } else {
+                    return UserModel.createToken(
+                        // promise 中的每一个链条 都有一个独立的异步函数，每个回调函数都有一个独立的变量作用域。回调函数之间的变量不能共享
+                        // 即user 是上一个回调函数中的变量，不可以写在此处；
+                        // 关键字：如何在Promise链中共享变量？ 
+                        //  https://zhuanlan.zhihu.com/p/29052022
+                        {
+                            email: loginUser.email,
+                            userId: loginUser._id
+                        },
+                        'sceret',
+                        {
+                            expiresIn: "1h"
+                        }
+                    )
+                }
+            }
+        )
+        .then(
+            (token) => {
+                res.status(200)
+                    .json({
+                        message: "Auth successful",
+                        token: token
+                    })
+            }
+        ) 
+        .catch(
+            (err) => {
+                if (err.status) {
+                    res.status(err.status)
+                        .json({
+                            message: err.message
+                        })
+                } else {
+                    res.status(500)
+                    .json({
+                        error: err
+                    })
+                }
+            }
+        )
+})
+
+router.delete('/:userId', (req, res, next) => {
+    UserModel.deleteUser(req.params.userId)
+        .then(
+            (result) => {
+                res.status(200)
+                    .json({
+                        message: 'User deleted',
+                        result: result
+                    })
+                    .catch(
+                        (err) => {
+                            res.status(500)
+                                .json({
+                                    error: err
+                                })
+                        }
+                    )
+            }
+        )
+})
+
+module.exports = router;
